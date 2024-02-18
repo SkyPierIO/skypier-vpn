85de4dd768106c59baa8d594cd398a60dc0afdbf
smolgroot <nautilushell@protonmail.com>
2024-02-18 22:05:13 +0100

    set TUN interface down

--- controllers/tun.go
+++ controllers/tun.go
@@ -8,19 +8,23 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
+var interfaceName = "skypier0"
+
 func SetInterfaceUp() {
 
 	config := water.Config{
 		DeviceType: water.TAP,
 	}
-	config.Name = "skypier0"
+	config.Name = interfaceName
 
+	// Create a new TUN/TAP interface using config.
 	iface, err := water.New(config)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pierIface, _ := netlink.LinkByName("skypier0")
+	// Configure the network interface
+	pierIface, _ := netlink.LinkByName(interfaceName)
 	addr, _ := netlink.ParseAddr("10.1.1.1/24") // TODO remove static IP
 	netlink.AddrAdd(pierIface, addr)
 	netlink.LinkSetUp(pierIface)
@@ -34,13 +38,17 @@ func SetInterfaceUp() {
 			log.Fatal(err)
 		}
 		frame = frame[:n]
-		// log.Printf("Dst: %s\n", frame.Destination())
-		// log.Printf("Src: %s\n", frame.Source())
-		// log.Printf("Ethertype: % x\n", frame.Ethertype())
-		// log.Printf("Payload: % x\n", frame.Payload())
+		log.Printf("Dst: %s\n", frame.Destination())
+		log.Printf("Src: %s\n", frame.Source())
+		log.Printf("Ethertype: % x\n", frame.Ethertype())
+		log.Printf("Payload: % x\n", frame.Payload())
 	}
 }
 
-func SetInterfaceDown() {
-	// TODO implement it
+func SetInterfaceDown() error {
+	link, err := netlink.LinkByName(interfaceName)
+	if err != nil {
+		return err
+	}
+	return netlink.LinkSetDown(link)
 }
