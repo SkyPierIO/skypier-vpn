218297ae6d9ba472e6d3258f5891361eb67d0567
smolgroot <nautilushell@protonmail.com>
2024-07-27 01:35:07 +0200

    🐛 debugging Gin endpoint for the VPN Connect

--- pkg/vpn/p2pEndpoints.go
+++ pkg/vpn/p2pEndpoints.go
@@ -1,7 +1,6 @@
 package vpn
 
 import (
-	"context"
 	"encoding/binary"
 	"fmt"
 	"log"
@@ -146,51 +145,48 @@ func Connect(node host.Host, dht *dht.IpfsDHT) gin.HandlerFunc {
 			c.IndentedJSON(200, err)
 			return
 		}
-		log.Println("Connected to the remote peer", pi.ID)
-
-		// we can open a new stream
-		s, err := node.NewStream(
-			context.Background(),
-			pi.ID,
-			"/skypier/1.0",
-		)
+		// we can open a new stream²
+		s, err := node.NewStream(c, pi.ID, "/skypier/1.0")
 		if err != nil {
 			log.Println(err)
 		}
 		iface := SetInterfaceUp()
-		res := fmt.Sprintf("Created a stream to the remote node and created `%v` VPN interface.", iface.Name())
 		type Result struct {
 			Res string `json:"result"`
 		}
-		r := &Result{Res: res}
-		c.IndentedJSON(200, r)
+		res := fmt.Sprintf("Connected to the destination & `%v` VPN interface created.", iface.Name())
+		log.Println(res)
+		c.IndentedJSON(200, Result{Res: res})
 		packet := make([]byte, 1420)
-		for {
-			plen, err := iface.Read(packet)
-			if err != nil {
-				break
-			}
-
-			// Write out the packet's length to the libp2p stream to ensure
-			// we know the full size of the packet at the other end.
-			err = binary.Write(s, binary.LittleEndian, uint16(plen))
-			if err == nil {
-				// Write the packet out to the libp2p stream.
-				// If everyting succeeds continue on to the next packet.
-				n, err := s.Write(packet[:plen])
-				fmt.Printf("Connected to the remote node %v, and sent %d bytes. %v\n", pi.ID, n, pi.Addrs)
-				if err == nil {
-					// debug
-					header, _ := ipv4.ParseHeader(packet[:plen])
-					fmt.Printf("Sending to remote: %+v (%+v)\n", header, err)
-					continue
+		loop := func() {
+			for {
+				plen, err := iface.Read(packet)
+				if err != nil {
+					break
 				}
-			}
-			// If we encounter an error when writing to a stream we should
-			// close that stream and delete it from the active stream map.
-			s.Close()
 
+				// Write out the packet's length to the libp2p stream to ensure
+				// we know the full size of the packet at the other end.
+				err = binary.Write(s, binary.LittleEndian, uint16(plen))
+				if err == nil {
+					// Write the packet out to the libp2p stream.
+					// If everyting succeeds continue on to the next packet.
+					n, err := s.Write(packet[:plen])
+					fmt.Printf("Connected to the remote node %v, and sent %d bytes. %v\n", pi.ID, n, pi.Addrs)
+					if err == nil {
+						// debug
+						header, _ := ipv4.ParseHeader(packet[:plen])
+						fmt.Printf("Sending to remote: %+v (%+v)\n", header, err)
+						continue
+					}
+				}
+				// If we encounter an error when writing to a stream we should
+				// close that stream and delete it from the active stream map.
+				s.Close()
+
+			}
 		}
+		go loop()
 	}
 	return gin.HandlerFunc(fn)
 }
